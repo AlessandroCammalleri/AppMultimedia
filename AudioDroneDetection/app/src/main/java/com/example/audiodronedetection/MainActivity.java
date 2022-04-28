bb621554019ee2470636bee040912b29d6b68e77
Alessandro Cammalleri <alessandro.cammalleri00@gmail.com>
2022-04-28 15:40:42 +0200

    Creazione progetto AndroidStudio

---
+++ AudioDroneDetection/app/src/main/java/com/example/audiodronedetection/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.audiodronedetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
