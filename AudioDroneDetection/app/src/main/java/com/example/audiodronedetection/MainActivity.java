32b700cabb8e7e768e42415fe2a0015e60377c8c
Alessio Bertini <alessio.bertini00@gmail.com>
2022-05-19 09:41:11 +0200

    Aggiunta activity con registratore audio

--- AudioDroneDetection/app/src/main/java/com/example/audiodronedetection/MainActivity.java
+++ AudioDroneDetection/app/src/main/java/com/example/audiodronedetection/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.audiodronedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bttDoRec = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bttDoRec = findViewById(R.id.bttDoRec);
+
+        bttDoRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getString(R.string.LAUNCH_RECORDER));
+                startActivity(intent);
+            }
+        });
     }
 }
